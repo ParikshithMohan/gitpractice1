fce139d8a7456d354651dbd8376dadd5f9d959b6
kattakavya <kavya@front.live>
2019-01-14 22:00:35 +0530

    Changes

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gitpractice1
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gitpractice1/core_test.clj
@@ -0,0 +1,7 @@
+(ns gitpractice1.core-test
+  (:require [clojure.test :refer :all]
+            [gitpractice1.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
